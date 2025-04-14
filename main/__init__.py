a016f93f05b2cf3b438858698d8482f6c7ec8048
Dendop <denopod@gmail.com>
2025-04-14 11:54:46 +0100

    added icons

--- main/__init__.py
+++ main/__init__.py
@@ -9,6 +9,26 @@ BASE_WEATHER_API_URL = "http://api.openweathermap.org/data/2.5/weather"
 
 app = Flask(__name__)
 
+def get_icon_filename(weather_id):
+    if 200 <= weather_id < 300:
+        return 'thunder.svg'
+    elif 300 <= weather_id < 500:
+        return 'drizzle.svg'
+    elif 500 <= weather_id < 502:
+        return 'light_rain.svg'
+    elif 502 <= weather_id < 600:
+        return 'heavy_rain.svg'
+    elif 600 <= weather_id < 700:
+        return 'snow.svg'
+    elif 700 <= weather_id < 800:
+        return 'mist.svg'
+    elif weather_id == 800:
+        return 'clear_sky.svg'
+    elif weather_id > 800:
+        return 'clouds.svg'
+    else:
+        return f"something went wrong"
+
 @app.route("/", methods=["GET","POST"])
 def home():
     if request.method == "POST":
@@ -24,26 +44,26 @@ def home():
         try:
             
             response = requests.get(url)
-            #print(response.status_code)
+            
             
             data = response.json()
-            #print("Response JSON:", data)
+            
             
             #if success
             if response.status_code == 200:
                  
                 description = data['weather'][0]['description']
                 temperature = data['main']['temp']
-                weather_id = data['weather'][0]['id'] #for more advanced
-                #print("Description", description)
-                #print("temperature", temperature)
-                #print(city_name)
+                weather_id = data['weather'][0]['id'] #for more advanced "icon"
+                print("weather ID : ", weather_id)
+                
+                icon_filename = get_icon_filename(weather_id)
                 
                 return  jsonify({
                     "city_name": city_name,
                     "temperature": f"{temperature}°",
-                    "description": description
-                    
+                    "description": description,
+                    "icon_filename" : icon_filename
                 })
                 
             else:
--- main/static/js/main.js
+++ main/static/js/main.js
@@ -5,6 +5,13 @@ document.getElementById('weather-form').addEventListener('submit', function (eve
     let city = document.querySelector('[name="city"]').value;
     let units = document.querySelector('input[name="units"]:checked')?.value || 'metric';
 
+    const resultDiv = document.getElementById('result');
+    
+
+    //hide result
+    resultDiv.style.visibility = 'hidden';
+    resultDiv.innerHTML = "";
+ 
     // Send a POST request to the backend
     fetch('/', {
         method: 'POST',
@@ -17,15 +24,20 @@ document.getElementById('weather-form').addEventListener('submit', function (eve
     .then(data => {
         // Handle response data here
         if (data.temperature) {
+            const iconPath = `/static/images/icons/${data.icon_filename}`;
+
             document.getElementById('result').innerHTML = `
-                <h3>Weather in ${data.city_name}</h3>
-                <p>Temperature: ${data.temperature}</p>
-                <p>Weather: ${data.description}</p>
+                <h2>Today</h2>
+                <img src="${iconPath}" alt="weather icon" class = "weather-icon" />
+                <p>${data.temperature}${units === 'metric' ? 'C' : 'F'}</p>
+                <p>${data.description}</p>
+                
                 
             `;
         } else if (data.error_message) {
             document.getElementById('result').innerHTML = `<p>Error: ${data.error_message}</p>`;
         }
+        resultDiv.style.visibility = 'visible';
     })
     .catch(error => {
         console.error("Error:", error);
